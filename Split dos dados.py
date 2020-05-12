076677db927fab251e8e1537e8df68544c115675
Valesca Alves <61808567+valescaalvess@users.noreply.github.com>
2020-05-12 20:55:39 -0300

    Add files via upload

---
+++ Split dos dados.py
@@ -0,0 +1,10 @@
+# Fazendo split dos dados
+def split_string(text):
+    return text.split(" ")
+
+
+texto = 'Esta função será bastante útil para separar grandes volumes de dados.'
+
+# Isso divide a string em lista
+print(split_string(texto))
+
