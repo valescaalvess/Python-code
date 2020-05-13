83612a214a220a2033143fb05bbf9ba647d0c41e
Valesca Alves <61808567+valescaalvess@users.noreply.github.com>
2020-05-12 21:08:15 -0300

    Add files via upload

---
+++ Criando funções usando outras funções.py
@@ -0,0 +1,14 @@
+import math
+
+def numPrimo(num):
+    '''
+    Verificando se um número
+    é primo.
+    '''
+    if (num % 2) == 0 and num > 2:
+        return "Este número não é primo"
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        if (num % i) == 0:
+            return "Este número não é primo"
+    return "Este número é primo"
+numPrimo(541)
