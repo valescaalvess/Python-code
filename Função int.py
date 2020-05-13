ab561d33cd5d4be1fa48d910623490c3af8730c0
Valesca Alves <61808567+valescaalvess@users.noreply.github.com>
2020-05-12 21:07:51 -0300

    Add files via upload

---
+++ Função int.py
@@ -0,0 +1,15 @@
+#Usando a função int para converter o valor digitado
+idade = int(input('Digite sua idade:'))
+if idade >= 18:
+    print('Você pode dirigir.')
+else:
+    print('Você não pode dirigir com está idade.')
+
+#Usando função list
+list1 = [23, 25, 45, 46]
+sum(list1)
+
+
+
+
+
